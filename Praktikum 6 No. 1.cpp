81bf1dc26dd286858589f7c009dd98f90f544ed4
Firsta alina s <72842581+Firstaalinas@users.noreply.github.com>
2021-06-07 21:25:01 +0700

    Add files via upload

---
+++ Praktikum 6 No. 1.cpp
@@ -0,0 +1,18 @@
+Firsta Alina Saputra/20051397085/ D4 MI 2020A
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int n,i;
+	cout<<"===================================================\n";
+	cout<<"Proses Rekursi menulis angka dari n sampai dengan 0\n";
+	cout<<"===================================================\n\n";
+	cout<<"Masukkan sebuah angka n, n= ";
+	cin>>i;
+	
+	for (n=i; n>=0; n--){
+	cout<<"outputnya="<<n<<endl;
+   }
+}
